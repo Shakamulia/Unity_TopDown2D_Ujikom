bfd67e3359911ef44c33fd3d62ddcc1148d2e1b8
Shakamulia <satriacream45@gmail.com>
2025-02-11 02:35:05 +0700

    Arrow Projectile + Bow

--- Assets/Scripts/Player/DamageSource.cs
+++ Assets/Scripts/Player/DamageSource.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DamageSource : MonoBehaviour
 {
-    [SerializeField] private int damageAmount = 1; // Besar damage yang diberikan
+    [SerializeField] private int damageAmount = 1;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
