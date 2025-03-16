d1a31d3b8d322d3cca3b96beb066afd5cae9cfce
Shakamulia <satriacream45@gmail.com>
2025-03-16 23:02:19 +0700

    Master + Music Adjust Slider

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -8,12 +8,17 @@ public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider masterSlider;
 
     
     public void SetMusicVolume() //fungsi untuk mengatur volume musik
     {
        float volume = musicSlider.value;
         myMixer.SetFloat("Music", Mathf.Log10(volume)*20); //
+    }public void SetMasterVolume() //fungsi untuk mengatur volume musik
+    {
+       float volume = masterSlider.value;
+        myMixer.SetFloat("Master", Mathf.Log10(volume)*20); //
     }
     
 }
